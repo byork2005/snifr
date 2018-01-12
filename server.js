267b758a46b828d0f5809a206b7ec13a6099efbc
David Fulton <fultonium@gmail.com>
2018-01-12 00:40:50 +0000

    package and server file

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT;
+var db = require("./models");
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./routes/api.js");
+app.use("/", routes);
+
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+  });
+});
+
