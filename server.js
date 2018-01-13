85e2b10e1a5d090279e3f86474b6656eab744ec1
David Fulton <fultonium@gmail.com>
2018-01-13 19:02:35 +0000

    passport boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require("passport");
+var Strategy = require('passport-local').Strategy;
 
 var port = process.env.PORT;
 var db = require("./models");
@@ -9,6 +11,34 @@ var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 var exphbs = require("express-handlebars");
 
