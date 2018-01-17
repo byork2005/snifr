bdfa4ba87a20b492d3da82f047c0519ad2077ebd
David Fulton <fultonium@gmail.com>
2018-01-17 14:50:30 +0000

    moved passport code to separate file

--- server.js
+++ server.js
@@ -12,40 +12,12 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.User.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-passport.serializeUser(function(user, cb) {
-  console.log("ser user")
-  console.log(user.id);
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  console.log("des prob")
-  console.log(id);
-  db.User.findById(id).then(function(user){
-    if(user){
-      console.log("found")
-      cb(null, user.get());
-    } else {
-      cb(user.errors, null);
-    }
-  });
-});
-
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false}));
 
+require('./config/passport')(passport, db.User);
 app.use(passport.initialize());
 app.use(passport.session());
 
