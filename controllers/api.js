27f638b0dba479ff098163c06367e0d96e4b61a5
David Fulton <fultonium@gmail.com>
2018-01-17 14:49:18 +0000

    added auth check

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/users', function(req, res){
+var ensureAuth = function(req, res, next){
+    console.log("authing");
+    if (req.isAuthenticated()){
+        console.log("authed");
+        return next();
+    } else {
+        res.status(401).json({message: 'Access Denied'});
+    }
+};
+
+router.get('/users', ensureAuth, function(req, res){
     res.json({status: "success"});
 });
 
