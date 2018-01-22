641a42ce1bf797f90df6aeb089e15e35e3d7410b
Harry Spencer <haspencer4@gmail.com>
2018-01-22 17:16:46 -0600

    foundation routes built

--- controllers/client.js
+++ controllers/client.js
@@ -12,28 +12,62 @@ var ensureAuth = function(req, res, next){
     }
 };
 
+//handlebars
+//routes for the websites, this is all tenative
+//removed the ensureAuth to get working
 router.get('/', function(req, res){
     var hbsObj = {status: "success"};
     //res.render('index', hbsObj);
-    res.sendfile('./views/templates/login.html')
+    res.render('login')
 });
 
-router.get('/profile', ensureAuth, function(req, res){
-    res.sendfile('./views/templates/profile.html')
+router.get('/signup1', function(req, res){
+    res.render('signupHumanInfo');
+});
+
+router.get('/signup2', function(req, res){
+    res.render('signupDogInfo');
+});
+
+router.get('/signup3', function(req, res){
+    res.render('signupSurvey');
+});
+
+router.get('/signup4', function(req, res){
+    res.render('signupFilters');
+});
+
+router.get('/home', function(req, res){
+    res.render('homePage');
+});
+
+router.get('/matches', function(req, res){
+    res.render('matchPage');
+});
+
+router.get('/profile', function(req, res){
+    res.render('profile')
 })
+
+router.get('/barks', function(req, res){
+    res.render('barksPage')
+})
+//end of handlebars routes
 
 router.get('/signin', function(req, res){
     var hbsObj = {};
     res.render('signin', hbsObj);
 });
 
-router.get('/survey1', function(req, res){
-    res.sendfile('./views/templates/survey1.html');
-});
 
-router.get('/survey2', ensureAuth, function(req, res){
-    res.sendfile('./views/templates/survey2.html');
-})
+//older routes before handlebars
+// router.get('/survey1', function(req, res){
+//     res.sendfile('./views/templates/survey1.html');
+// });
+
+// router.get('/survey2', ensureAuth, function(req, res){
+//     res.sendfile('./views/templates/survey2.html');
+// })
 
 router.post('/signin',
             passport.authenticate('local-signin', {
