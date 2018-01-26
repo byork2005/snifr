f6313a7ff5ad5f8242a08f4e2960f6f7ede0e471
unknown <suhail.purewal@gmail.com>
2018-01-25 19:05:10 -0600

    okay

--- controllers/client.js
+++ controllers/client.js
@@ -184,8 +184,38 @@ router.get('/update/filter', function(req, res){
     res.render('updateFilter');
 });
 
-router.get('/barks', function (req, res) {
-    res.render('barksPage');
+// MAIN BARKS PAGE - should have all incoming/outgoing barks - but only display the dogs - not the actual convo
+router.get('/barks', ensureAuth, function (req, res) {
+    models.Communication.findAll({
+        where: {
+        [Op.or]: [{initiator_id: req.user.id}, {receiver_id: req.user.id}]
+        //need to limit it to 1 instance of each person
+        },
+        include: [models.Dog]
+    }).then(function (data) {
+        // console.log(data);
+        // let userInfo = data.get();
+        // console.log(data)
+        // console.log(data.Dogs)
+        res.render('barksPage', { Dog: data.Dogs });
+    });
+});
+
+// CONVO PAGE - when you click the convo on the main page - it should take you to the chat which shows your chats with specific user you clicked
+router.get('/barks/:otherDog', ensureAuth, function (req, res) {
+    models.Communication.findAll({
+        where: {
+            [Op.or]: [{initiator_id: req.user.id}, {receiver_id: req.user.id}],
+            [Op.or]: [{initiator_id: req.params.otherDog}, {receiver_id: req.params.otherDog}]
+            },
+            include: [models.Dog]
+    }).then(function (data) {
+        // console.log(data);
+        // let userInfo = data.get();
+        // console.log(data)
+        // console.log(data.Dogs)
+        res.render('barksMsgsPage', { Dog: data.Dogs });
+    });
 });
 //end of handlebars routes
 
